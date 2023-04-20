6a801989e301f5f2458c36daffb876c02a5758a5
Tai-Rocha <taina013@gmail.com>
2023-04-20 12:00:18 -0300

    estduos de casos

---
+++ R/VII SER.R
@@ -0,0 +1,31 @@
+#################################################################
+## VII Seminário Internacional de Estatística com R (SER)
+## Manipulação e estatística básica de dados geoespaciais no R
+## Autora: Tainá Rocha
+################################################################
+
+
+# Estudo de caso 1: Cobertura vacinal para o Estado do Rio de Janeiro 
+# openDataSUS: https://opendatasus.saude.gov.br/dataset/covid-19-vacinacao/resource/10aed154-04c8-4cf4-b78a-8f0fa1bc5af4  
+# Dados RJ parte 5
+
+
+# Library
+library(dplyr) # manipulação de dataframes
+library(ggplot2) # visualização de dados
+library(sf) # simple feature uma maneira padronizada de codificar dados vetoriais espaciais
+
+
+
+
+
+
+# Estudo de caso 2 - Precipitação BR
+
+# Dados de precipitação do wordlim (precipitation (mm) 5 minutes)- https://www.worldclim.org/data/worldclim21.html#google_vignette
+
+library(sf)    # simple feature uma maneira padronizada de codificar dados vetoriais espaciais         
+library(terra)          
+library(ggplot2)   
+
+
